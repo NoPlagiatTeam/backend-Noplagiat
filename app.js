27d6c009b2fed2439c1a859ca433f9070adda362
DavePhil <104580100+DavePhil@users.noreply.github.com>
2024-05-26 14:41:55 +0100

    Ajout des fichiers pour le deploiement continue

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const traitementRouter = require('./src/routes/TraitementRouter')
@@ -14,11 +13,11 @@ const sequelize = require('./src/db/sequelize');
 const port = normalizePort(process.env.PORT || '5000');
 
 const app = express();
+
 app.use(bodyParser.json())
 app.use(logger('dev'));
 // app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
