51a5a7d8ff5dd7ae8470a0029529df28a169d029
DavePhil <104580100+DavePhil@users.noreply.github.com>
2024-05-26 14:57:52 +0100

    Ajout des fichiers pour le deploiement continue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const traitementRouter = require('./src/routes/TraitementRouter')
@@ -9,26 +10,28 @@ const rapportRouter = require('./src/routes/RapportRouter')
 const souscriptionRouter = require('./src/routes/SouscriptionRouter')
 const tokenRouter = require('./src/routes/TokenRouter')
 const userRouter = require('./src/routes/UserRouter')
-// const sequelize = require('./src/db/sequelize');
-const port = normalizePort(process.env.PORT || '5000');
+const sequelize = require('./src/db/sequelize');
 
 const app = express();
-
-// sequelize.initDB().then(r => console.log("Connexion à la BD effectuée"));
-
 app.use(bodyParser.json())
 app.use(logger('dev'));
 // app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+
+
+
 app.use('/downloaded_pdfs',express.static(__dirname + '/downloaded_pdfs'))
 app.use('/upload_docs',express.static(__dirname + '/upload_docs'))
+
+sequelize.initDB();
 
 // Liste des routes du serveur
 app.use('/api/', traitementRouter);
@@ -44,7 +47,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -55,25 +58,5 @@ app.use(function(err, req, res) {
     error: err
   });
 });
-
-app.listen(port, () => {
-  console.log(`L'API est en cours d'exécution sur http://localhost:${port}`);
-});
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
 
 module.exports = app;
