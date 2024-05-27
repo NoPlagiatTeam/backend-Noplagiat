ecec8181d560bcab5bd3b04c6eb372e08c29ca29
DavePhil <104580100+DavePhil@users.noreply.github.com>
2024-05-27 16:47:52 +0100

    Ajout des fichiers pour le deploiement continue

--- Algorithm/GoogleResearch/searchGoogleForPDFs.js
+++ Algorithm/GoogleResearch/searchGoogleForPDFs.js
@@ -7,20 +7,19 @@ async function searchGoogleForPDFs(query) {
     console.log(`https://www.google.com/search?q=${encodeURIComponent(formatText(query))}&num=10&filter=0`)
       try {
         const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true,
-    ignoreHTTPSErrors: true,
-    defaultViewport: null,
-    args: ['--start-maximized'],
-  });
-  const page = await browser.newPage();
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+            args: ['--start-maximized'],
+            headless: true,
+            ignoreHTTPSErrors: true,
+            defaultViewport: null,
+        });
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
   
         // Effectuer une recherche Google avec la requête
         await page.goto(`https://www.google.com/search?q=${encodeURIComponent(query)}&num=10&filter=0`);
         
         // Extraire les liens pertinents pointant vers des fichiers PDF
-        var ct =await page.content();
+        const ct =await page.content();
         console.log(ct.toString())
         const pdfLinks = await page.evaluate(() => {
           const links = Array.from(document.querySelectorAll('a'));
